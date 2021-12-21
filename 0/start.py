eccdd428b4928298e6813c03f8df9ca85009bd85
jesusjorge <35180978+jesusjorge@users.noreply.github.com>
2021-12-21 16:46:46 -0700

    Update start.py

--- 0/start.py
+++ 0/start.py
@@ -1,11 +1,39 @@
-from tkinter import *
+import json
+import subprocess
+import urllib.request
+import webbrowser
 
-root = Tk()
+class ju:
+    pass
+    pretext = "https://raw.githubusercontent.com/jesusjorge/docs/main/0/"
+    def ci(imp,install):
+        try:
+            exec(imp,globals())
+        except:
+            try:
+                result = subprocess.check_output("pip3 install " + install,shell=True,stderr=subprocess.STDOUT)
+                exec(imp,globals())
+            except Exception as e:
+                error = e.output.decode()
+                print(error)
+                webbrowser.open("https://visualstudio.microsoft.com/es/visual-cpp-build-tools/")
+                raise Exception(error)
+    def go(url):
+        return urllib.request.urlopen(url).read()
+    def son(s):
+        return json.loads(s)
 
-for r in range(0, 5):
-    for c in range(0, 5):
-        cell = Entry(root, width=30)
-        cell.grid(row=r, column=c)
-        cell.insert(0, '({}, {})'.format(r, c))
+menu = ju.son(ju.go(ju.pretext + "menu.json"))
+while True:
+    print("\nChoose an option:")
+    c = 0
+    for it in menu:
+        print("[" + str(c) + "] " + it[0])
+        c = c + 1
+    option = input("Option: ")
+    try:
+        code = menu[int(option)][1]
+        exec(code,globals())
+    except:
+        print("Invalid Option!")
 
-root.mainloop()
