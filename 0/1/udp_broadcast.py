c788a84e3875ff9ff83b23f1db77b1a49a53995e
jesusjorge <35180978+jesusjorge@users.noreply.github.com>
2021-12-21 16:40:25 -0700

    Update udp_broadcast.py

--- 0/1/udp_broadcast.py
+++ 0/1/udp_broadcast.py
@@ -1,16 +1,18 @@
+ju.ci("import psutil","psutil")
+
 from socket import *
-import time
-from datetime import datetime
-import psutil
-s=socket(AF_INET, SOCK_DGRAM)
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-def Log(message):
-    print("Broadcast: " + message)
-    s.sendto(message.encode('utf-8'),('255.255.255.255',12345))
-while True:
-    msg = gethostname() + \
-        ", Datetime " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + \
-        ", CPU " + str(psutil.cpu_percent()) + "%" + \
-        ", RAM " + str(psutil.virtual_memory().percent) + "%"
-    Log(msg)
-    time.sleep(5)
+    import time
+    from datetime import datetime
+
+    s=socket(AF_INET, SOCK_DGRAM)
+    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    def Log(message):
+        print("Broadcast: " + message)
+        s.sendto(message.encode('utf-8'),('255.255.255.255',12345))
+    while True:
+        msg = gethostname() + \
+            ", Datetime " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + \
+            ", CPU " + str(psutil.cpu_percent()) + "%" + \
+            ", RAM " + str(psutil.virtual_memory().percent) + "%"
+        Log(msg)
+        time.sleep(5)
