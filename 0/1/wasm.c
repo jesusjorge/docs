3789d0d34f2b37b6ad1ae0861f71cf077342c87c
jesusjorge <35180978+jesusjorge@users.noreply.github.com>
2021-12-22 16:40:29 -0700

    Update wasm.c

--- 0/1/wasm.c
+++ 0/1/wasm.c
@@ -1,30 +1,29 @@
+void print(char* msg);  //function prototype
+
 //poor man's malloc
-int MALLOC_INDEX = 65536;
-void* malloc(int size)
-{
-  MALLOC_INDEX = MALLOC_INDEX - size;
-  return MALLOC_INDEX;
+int MALLOC_PTR = 65536;
+void* malloc(int size){
+  MALLOC_PTR = MALLOC_PTR - size;
+  return MALLOC_PTR;
 }
 
-void mem_cpy(char* destiny, int destiny_index, char* source, int source_index, int length)
-{
+void mem_cpy(char* destiny, int destiny_index, char* source, int source_index, int length){
   for(int i = 0; i < length; i++)
     destiny[destiny_index + i] = source[source_index + i]; 
 }
 
-int str_len(char* str)
-{
+int str_len(char* str){
   int c = 0;
   while(str[c] != 0)
     c = c + 1;
   return c;
 }
 
-char* str_concat(char* a, char* b)
-{
+char* str_concat(char* a, char* b){
   int la = str_len(a);
   int lb = str_len(b);
   char* result = malloc(la + lb + 1);
+  result[la + lb] = 0;
   mem_cpy(result,0,a,0,la);
   mem_cpy(result,la,b,0,lb);
   return result;
@@ -34,11 +33,11 @@ char a[] = "Hola ";
 char b[] = "Mundo!";
 
 char* demo(){
+  print("Ejecutando");
   return str_concat(a,b);
 }
 
 int main() { 
-  int x = malloc(10);
-  return x;
+  print("Iniciando");
   return str_len("ABC");
 }
